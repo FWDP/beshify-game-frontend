84c09a811e2d1198a6e72b3d0ccf014063478fef
Francis Ong <francis.ong25@gmail.com>
2023-07-13 22:38:03 +0800

    feat: add route config and apply routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import AppRoutes from './routes';
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Beshify Game</h1>
+      {/* Modals Here */}
+      <AppRoutes />
     </>
   );
 }
