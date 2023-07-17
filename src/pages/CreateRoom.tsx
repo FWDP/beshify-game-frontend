5e462da00da8b66c17a322a18f491ee521026cb4
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2023-07-17 22:54:08 +0800

    feat(createroom.tsx): create a basic form (time limit not yet included) for creating rooms

--- src/pages/CreateRoom.tsx
+++ src/pages/CreateRoom.tsx
@@ -1,5 +1,58 @@
+import { useForm } from "react-hook-form";
+
 const CreateRoom = () => {
-  return <div>CreateRoom</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((d) => console.log(d));
+
+  return (
+    <div className="flex justify-center">
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+      <form onSubmit={onSubmit} className="text-center">
+        <h1 className="font-bold text-3xl m-8">CREATE ROOM</h1>
+        <label className="flex flex-col">
+          ✨Room🤸name🤸ng🤸mga🤸beshie✨
+          <input
+            type="text"
+            placeholder="Room Name"
+            className="input input-bordered input-sm w-full max-w-xs my-3"
+            {...register("roomName")}
+          />
+        </label>
+        <label className="flex flex-col">
+          ✨Password✨
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered input-sm w-full max-w-xs my-3"
+            {...register("password")}
+          />
+        </label>
+        <label className="flex flex-col">
+          ✨Room Size✨
+          <input
+            type="text"
+            placeholder="Room Size"
+            className="input input-bordered input-sm w-full max-w-xs my-3"
+            {...register("roomSize")}
+          />
+        </label>
+        <div>✨Time🤸to🤸beshify✨</div>
+        {/* <label className="flex flex-col">
+          <input
+            type="text"
+            placeholder="Room Size"
+            className="input input-bordered input-sm w-full max-w-xs"
+            {...register("roomSize")}
+          />
+        </label> */}
+
+        <button type="submit" className="btn bg-green-600 text-white">
+          PLAY NA!
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateRoom;
