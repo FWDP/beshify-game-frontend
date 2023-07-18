2a13ff0a2727e7f50919bfe1353768d92e624dc1
Francis Ong <francis.ong25@gmail.com>
2023-07-18 22:28:52 +0800

    feat: add sample data for roomdetails

---
+++ src/constants/index.ts
@@ -0,0 +1,32 @@
+import { LobbyDetails } from '../types';
+
+export const roomData: LobbyDetails[] = [
+  {
+    roomId: 'room-1',
+    roomName: 'Beshimoms',
+    avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png',
+    playerCount: 42,
+    roomSize: 50,
+  },
+  {
+    roomId: 'room-2',
+    roomName: 'XOXO',
+    avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png',
+    playerCount: 2,
+    roomSize: 50,
+  },
+  {
+    roomId: 'room-3',
+    roomName: 'Gengineer',
+    avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png',
+    playerCount: 50,
+    roomSize: 50,
+  },
+  {
+    roomId: 'room-4',
+    roomName: 'xddDD',
+    avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png',
+    playerCount: 16,
+    roomSize: 50,
+  },
+];
