89dc6cd27a84fb0a66a755f67ed14367d2d7e207
RedDotz20 <carlos_tabangay@outlook.com>
2023-07-21 17:36:25 +0800

    refactor: lobby logo add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,31 @@
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <header className="navbar bg-base-100 shadow-md">
-      <div className="flex-1">
-        <Link className="normal-case text-xl font-semibold px-3" to="/">
-          Beshify
-        </Link>
-      </div>
-      <nav className="flex-none">
-        <ul className="menu menu-horizontal px-1 uppercase font-semibold">
-          <li>
-            <Link to="/how-to-play">How to play</Link>
-          </li>
-          <li>
-            <Link to="/about">How to play</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact us</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="shadow-md navbar bg-base-100">
+			<div className="flex-1">
+				<Link
+					className="px-3 text-xl font-semibold normal-case"
+					to="/"
+				>
+					Beshify
+				</Link>
+			</div>
+			<nav className="flex-none">
+				<ul className="px-1 font-semibold uppercase menu menu-horizontal">
+					<li>
+						<Link to="/how-to-play">How to play</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact us</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,62 +1,79 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { LiaGreaterThanSolid, LiaLessThanSolid } from 'react-icons/lia';
 import { useNavigate } from 'react-router-dom';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import logo from '../assets/dev-dino-blue.png';
 
 import { User } from '../types';
 
 const Home = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<User>();
-  const navigate = useNavigate();
-  const onSubmit: SubmitHandler<User> = (data) => {
-    const targetButton = document.activeElement as HTMLButtonElement | null;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<User>();
+	const navigate = useNavigate();
+	const onSubmit: SubmitHandler<User> = (data) => {
+		const targetButton = document.activeElement as HTMLButtonElement | null;
 
-    if (targetButton?.id) {
-      alert(`Welcome ${data.username}!`);
-      navigate(targetButton.id);
-    }
-  };
+		if (targetButton?.id) {
+			alert(`Welcome ${data.username}!`);
+			navigate(targetButton.id);
+		}
+	};
 
-  return (
-    <main className="flex items-center justify-center h-[80vh]">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1 className="text-3xl my-2 font-semibold">Ano🤸pangalan🤸mo🤸beshie?</h1>
-        <div className="my-5">
-          <input
-            type="text"
-            placeholder="Username"
-            {...register('username', { required: true })}
-            className="input input-bordered w-full text-center "
-          />
-          {errors.username && (
-            <label className="label">
-              <span className="label-text-alt text-red-500 text-center w-full">
-                This field is required
-              </span>
-            </label>
-          )}
-        </div>
-        <div className="flex flex-col gap-4 font-semibold">
-          <button
-            type="submit"
-            id="create-room"
-            className="btn-success text-white uppercase py-2 px-3 rounded"
-          >
-            Create Room
-          </button>
-          <button
-            type="submit"
-            id="lobby"
-            className="btn-info text-white uppercase py-2 px-3 rounded"
-          >
-            Join Room
-          </button>
-        </div>
-      </form>
-    </main>
-  );
+	return (
+		<main className="flex items-center justify-center h-[80vh]">
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="flex flex-col items-center justify-center"
+			>
+				<h1 className="my-2 mb-12 text-3xl font-semibold">
+					Ano🤸pangalan🤸mo🤸beshie?
+				</h1>
+				<div className="flex items-center gap-8">
+					<LiaLessThanSolid size={28} />
+					<img
+						className="w-[130px] mb-4"
+						src={logo}
+						alt="logo"
+					/>
+					<LiaGreaterThanSolid size={28} />
+				</div>
+
+				<input
+					type="text"
+					placeholder="Username"
+					{...register('username', { required: true })}
+					className="w-full text-center input input-bordered "
+				/>
+				<label className="label">
+					{errors.username ? (
+						<span className="w-full my-1 text-center text-red-500 label-text-alt">
+							This field is required
+						</span>
+					) : (
+						<span className="my-1"></span>
+					)}
+				</label>
+
+				<button
+					type="submit"
+					id="create-room"
+					className="w-48 px-3 py-2 mb-4 font-black text-white uppercase rounded btn-success"
+				>
+					Create Room
+				</button>
+
+				<button
+					type="submit"
+					id="lobby"
+					className="w-48 px-3 py-2 font-black text-white uppercase rounded btn-info"
+				>
+					Join Room
+				</button>
+			</form>
+		</main>
+	);
 };
 
 export default Home;
