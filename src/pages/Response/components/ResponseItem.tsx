a055ba452e07b1ac06da1cbd4f5dd3c417ad1928
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-07-23 20:53:44 +0800

    style: hover effects to response item and action buttons

--- src/pages/Response/components/ResponseItem.tsx
+++ src/pages/Response/components/ResponseItem.tsx
@@ -8,7 +8,7 @@ type TProps = {
 
 const ResponseItem: React.FC<TProps> = ({ response }) => {
   return (
-    <div className="h-[250px] p-4 bg-gray-50 border shadow-sm rounded flex flex-col">
+    <div className="h-[250px] p-4 bg-gray-50 border shadow-sm rounded flex flex-col hover:bg-gray-100 hover:-translate-y-1 transition-all duration-200">
       {/* Username Container */}
       <div>
         <span className="text-lg font-bold">{response.username}</span>
@@ -19,13 +19,13 @@ const ResponseItem: React.FC<TProps> = ({ response }) => {
       </div>
       {/* Actions Container */}
       <div className="justify-end self-end flex flex-row gap-2">
-        <button className="bg-gray-200 h-10 w-10 rounded-full aspect-square">
+        <button className="bg-gray-200 h-10 w-10 rounded-full aspect-square hover:bg-gray-300 hover:border hover:border-gray-500 transition-all duration-200">
           🤸🏽
         </button>
-        <button className="bg-gray-200 h-10 w-10 rounded-full aspect-square">
+        <button className="bg-gray-200 h-10 w-10 rounded-full aspect-square hover:bg-gray-300 hover:border hover:border-gray-500 transition-all duration-200">
           ✨
         </button>
-        <button className="bg-gray-200 h-10 w-10 rounded-full aspect-square">
+        <button className="bg-gray-200 h-10 w-10 rounded-full aspect-square hover:bg-gray-300 hover:border hover:border-gray-500 transition-all duration-200">
           ❌
         </button>
       </div>
